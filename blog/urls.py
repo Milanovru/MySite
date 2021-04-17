5693b1ba4dbff19e12edb661203a17709a847b85
pavel.milanov <pawel.milanov@uandex.ru>
2021-04-17 14:57:37 +0300

    представления реализованы с помощью ListView и DeatailView

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Homepage.as_view(), name='hello_url'),
-    path('portfolio/', views.Portfolio.as_view(), name='posts_url'),
-    path('post/<int:pk>/', views.Posts_id.as_view(), name='post_id_url')
+    path('portfolio/', views.PostList.as_view(), name='posts_url'),
+    path('post/<int:pk>/', views.PostDetail.as_view(), name='post_id_url')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView, DetailView
 from . models import Post
 
 
-# Create your views here.
 class Homepage(View):
     def get(self, request):
         return render(request, 'blog/main.html')
 
 
-class Portfolio(View):
-    def get(self, request):
-        posts = Post.objects.all()
-        return render(request, 'blog/portfolio.html', context={'posts': posts})
+class PostList(ListView):
+    model = Post # или queryset = Post.object.all()
+    context_object_name = 'posts'
+    template_name = 'blog/portfolio.html'
+# class Portfolio(View):
+#     def get(self, request):
+#         posts = Post.objects.all()
+#         return render(request, 'blog/portfolio.html', context={'posts': posts})
 
 
-class Posts_id(View):
-    def get(self, request, pk):
-        id = Post.objects.get(pk=pk)
-        return render(request, 'blog/post_page.html', context={'post': id}) # элемент в цикле
-
-
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'blog/post_page.html'
+# class Posts_id(View):
+#     def get(self, request, pk):
+#         id = Post.objects.get(pk=pk)
+#         # элемент в цикле
+#         return render(request, 'blog/post_page.html', context={'post': id})
